07b55274e0c3153d653617b43d7dbba91324d60c
miles waugh <miles.m.waugh@gmail.com>
2022-11-10 23:38:04 -0800

    Update

--- main.py
+++ main.py
@@ -2,31 +2,33 @@ from pytube import YouTube
 from moviepy.editor import *
 import os
 
-url = "https://www.youtube.com/watch?v=v-8h00D6l8E"
+print('Enter URL')
+url = input()
 
 i = 1
 j = str(i)
+
 try:
-    video = YouTube(url)
+    video = YouTube(url).streams.get_highest_resolution()
+    t = video.title
+    print('Downloading '+t)
+    video.download(filename='d-'+j+'.mp4')
 
-    title = video.title
-    video = video.streams.get_highest_resolution()
-
-    print("Downloading "+title)
-    video.download(filename='d-'+j+".mp4")
 except Exception as e:
-    print("Failed to download video.")
+    print('Failed to download video.')
     print(e)
+
 else:
     try:
-        videoclip = VideoFileClip('d-'+j+".mp4")
+        videoclip = VideoFileClip('d-'+j+'.mp4')
         audioclip = videoclip.audio
-        audioclip.write_audiofile(title+".mp3")
+        audioclip.write_audiofile(t+'.mp3')
         audioclip.close()
         videoclip.close()
     except Exception as r:
-        print("Failed to convert to mp3.")
+        print('Failed to convert to mp3.')
         print(r)
     else:
-        print("Deleting interim files")
-        os.remove("d-"+j+".mp4")
+        print('Deleting interim files')
+        os.remove('d-'+j+'.mp4')
+        print('Done')
